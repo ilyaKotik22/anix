97255ad4a77738310c538dbff97dbf37f61bb5d7
ilyakotik22 <ilyakotik2204@gmail.com>
2026-02-01 18:13:51 +0300

    add desctop ui

--- myapp/src/components/layout/header/header.tsx
+++ myapp/src/components/layout/header/header.tsx
@@ -1,7 +1,7 @@
 import { Link, useLocation } from 'react-router-dom';
 import style from './header.module.css'
-import { MyInput } from '@/components/ui/myInput/MyInput';
 import SearchInput from '@/features/searchInput/SearcInput';
+import MyButton from '@/components/ui/myButton/MyButton';
 const Header = () => {
     const location = useLocation()
     console.log(location)
@@ -21,17 +21,19 @@ const Header = () => {
                 
                 <section className={style.rightBar}>
                     <div className="">нст</div>
-                    <div className="">профиль</div>
+                    <MyButton content='Войти'/>
                 </section>
                 
             </section>
             <section className={style.sec}>
+                
                 <ul>
                     {navigation.map((el) => 
                         <li key={el.label}>
                             <Link className={location.pathname === el.path ? style.linkPick : style.link } to={el.path}>{el.label}</Link>
                         </li>)}
                 </ul>
+                <div className={style.logo}>logo</div>
             </section>
         </header> );
 }
